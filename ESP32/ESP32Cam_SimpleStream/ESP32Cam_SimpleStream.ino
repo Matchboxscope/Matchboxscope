8a66927a9d7101c76a1ee12928209522a55ce04c
beniroquai <bene.d@gmx.de>
2022-02-27 14:15:29 -0800

    Attempting to make the CSS files working offline

--- ESP32/ESP32Cam_SimpleStream/ESP32Cam_SimpleStream.ino
+++ ESP32/ESP32Cam_SimpleStream/ESP32Cam_SimpleStream.ino
@@ -1,7 +1,7 @@
 //IMPORTANT: ENABLE PSRAM!
-
 #include <esp32cam.h>
 #include <WebServer.h>
+#include <ESPAsyncWebServer.h>
 #include <WiFi.h>
 #include <SPIFFS.h>
 #include <FS.h>
@@ -9,39 +9,80 @@
 //const char *SSID = "ATTNVezUEM";
 //const char *PWD = "t8bfmze5a#id";
 
-
-const char *SSID = "Blynk";
-const char *PWD = "12345678";
 const char *SSID_AP = "MatchboxScope";
 
-WebServer server(80);
+AsyncWebServer asyncserver(80);
+WebServer server(81);
 
 static auto loRes = esp32cam::Resolution::find(320, 240);
 static auto hiRes = esp32cam::Resolution::find(800, 600);
-static auto maxRes = esp32cam::Resolution::find(1600,1200);
+static auto maxRes = esp32cam::Resolution::find(1600, 1200);
+
+
+void initSpiffs()
+{
+  if (!SPIFFS.begin()) /* Démarrage du gestionnaire de fichiers SPIFFS */
+  {
+    Serial.println("Erreur SPIFFS...");
+    return;
+  }
+
+  /* Détection des fichiers présents sur l'Esp32 */
+  File root = SPIFFS.open("/");    /* Ouverture de la racine */
+  File file = root.openNextFile(); /* Ouverture du 1er fichier */
+  while (file)                     /* Boucle de test de présence des fichiers - Si plus de fichiers la boucle s'arrête*/
+
+  {
+    Serial.print("File: ");
+    Serial.println(file.name());
+    file.close();
+    file = root.openNextFile(); /* Lecture du fichier suivant */
+  }
+}
+
+
+
+void initWebServerConfig()
+{
+
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  
+  asyncserver.on("/", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    Serial.println("get page");
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  asyncserver.on("/bootstrap.min.css", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    request->send(SPIFFS, "/bootstrap.min.css", "text/css");
+  });
+
+    asyncserver.on("/all.css", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    request->send(SPIFFS, "/all.css", "text/css");
+  });
+
+  server.on("/cam.bmp",handleBmp);
+  server.on("/cam-lo.jpg",handleJpgLo);
+  server.on("/cam-hhi.jpg",handleJpgHi);
+  server.on("/cam.jpg",handleJpg);
+  server.on("/cam.mjpg",handleMjpeg);
+
+  asyncserver.begin();
+  server.begin();
+}
+
 
 void connectToWiFi() {
-  Serial.print("Connecting to ");
-  Serial.println(SSID);
-
-  /*
-   * WiFi.begin(SSID, PWD);
-   * while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-    // we can even make the ESP32 to sleep
-  }
-
-  Serial.print("Connected. IP: ");
-  Serial.println(WiFi.localIP());
-
-   */
+  Serial.print("Network SSID: ");
+  Serial.println(SSID_AP);
 
   WiFi.softAP(SSID_AP);
-    IPAddress IP = WiFi.softAPIP();
+  IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
-  }
+}
 
 
 void handleBmp()
@@ -130,37 +171,6 @@ void handleMjpeg()
   Serial.printf("STREAM END %dfrm %0.2ffps\n", res, 1000.0 * res / duration);
 }
 
-void handleWebpage(){
-  Serial.println("Handle Webpage");
-  File file = SPIFFS.open("/index.html", "r");  
-  size_t sent = server.streamFile(file, "text/html");  
-  file.close();  
-  return;  
-}
-
-void handleStyle(){
-  Serial.println("Handle Webpage");
-  File file = SPIFFS.open("/style.css", "r");  
-  size_t sent = server.streamFile(file, "text/html");  
-  file.close();  
-  return;  
-}
-
-void handleBootstrapMin(){
-  Serial.println("Handle bootstrap.min.css");
-  File file = SPIFFS.open("/bootstrap.min.css", "r");  
-  size_t sent = server.streamFile(file, "text/html");  
-  file.close();  
-  return;  
-}
-
-void handleBootstrapAll(){
-  Serial.println("Handle all.css");
-  File file = SPIFFS.open("/all.css", "r");  
-  size_t sent = server.streamFile(file, "text/html");  
-  file.close();
-  return;  
-}
 
 
 
@@ -169,9 +179,9 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println();
-  
-  
- connectToWiFi();
+
+
+  connectToWiFi();
 
   {
     using namespace esp32cam;
@@ -186,10 +196,7 @@ void setup()
     Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
   }
 
-if(!SPIFFS.begin(true)){
-  Serial.println("An Error has occurred while mounting SPIFFS");
-  return;
-}
+  initSpiffs();
 
 
   Serial.print("http://");
@@ -202,25 +209,12 @@ if(!SPIFFS.begin(true)){
   Serial.println("  /style.css");
   Serial.println("  /all.css");
   Serial.println("  /BootstrapMin.css");
+  initWebServerConfig();
 
-  
-
-  server.on("/cam.bmp", handleBmp);
-  server.on("/cam-lo.jpg", handleJpgLo);
-  server.on("/cam-hi.jpg", handleJpgHi);
-  server.on("/cam.jpg", handleJpg);
-  server.on("/cam.mjpeg", handleMjpeg);
-  server.on("/index.html", handleWebpage);
-  server.on("/style.css", handleStyle);
-  server.on("/all.css", handleBootstrapAll);
-  server.on("/bootstrap.min.css", handleBootstrapMin);
-  server.on("/", handleWebpage);
-
-  server.begin();
-  Serial.println("START");
+  Serial.println("START SERVER");
 }
 
 void loop()
 {
-  server.handleClient();
+    server.handleClient();
 }
