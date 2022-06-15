f3786ee6ccd1088f951126869bc74df5221ace3f
Wei Ouyang <oeway007@gmail.com>
2022-06-15 23:58:16 +0200

    Update html.h

--- ESP32/main/html.h
+++ ESP32/main/html.h
@@ -67,16 +67,6 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
                     },
                 });
                 
-                function _base64ToArrayBuffer(base64) {
-                    var binary_string = window.atob(base64);
-                    var len = binary_string.length;
-                    var bytes = new Uint8Array(len);
-                    for (var i = 0; i < len; i++) {
-                        bytes[i] = binary_string.charCodeAt(i);
-                    }
-                    return bytes.buffer;
-                }
-                
                 window.sendToImageJ = async function(){
                     const response = await fetch(`${c}/capture.jpeg?_cb=${Date.now()}`);
                     const bytes = await response.arrayBuffer();
